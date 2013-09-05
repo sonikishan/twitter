705d657cf410c887c1c962b74039883f3dc1fe22
kishansoni <kishansoni@heraldsolutions.in>
2013-09-05 13:00:40 +0530

    User create and error/success messages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Twitter::Application.routes.draw do
   get "static_pages/about"
   get "static_pages/contact"
 
+  resources :users
+
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
